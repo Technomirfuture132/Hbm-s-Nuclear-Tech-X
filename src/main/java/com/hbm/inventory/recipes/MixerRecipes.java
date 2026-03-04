bbebc359ed72ecbdfab35318055f908ee62d42c0
Technomirfuture132 <141735756+Technomirfuture132@users.noreply.github.com>
2026-03-04 21:17:32 +0300

    Added curium salt production

--- src/main/java/com/hbm/inventory/recipes/MixerRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/MixerRecipes.java
@@ -76,6 +76,8 @@ public class MixerRecipes extends SerializableRecipe {
 				new MixerRecipe(1000, 20).setStack1(new FluidStack(Fluids.FISHOIL, 500)).setStack2(new FluidStack(Fluids.NITRIC_ACID, 500)));
 
 		register(Fluids.THORIUM_SALT, new MixerRecipe(1_000, 30).setStack1(new FluidStack(Fluids.CHLORINE, 1000)).setSolid(new OreDictStack(TH232.dust())));
+		
+		register(Fluids.CURIUM_SALT, new MixerRecipe(1_000, 30).setStack1(new FluidStack(Fluids.CHLORINE, 1000)).setSolid(new OreDictStack(CM.dust())));
 
 		register(Fluids.SYNGAS, new MixerRecipe(1_000, 50).setStack1(new FluidStack(Fluids.COALOIL, 500)).setStack2(new FluidStack(Fluids.STEAM, 500)));
 		register(Fluids.OXYHYDROGEN,
